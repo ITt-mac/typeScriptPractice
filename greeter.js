d226a024af84f06eadc49fcb8460504c3003389a
hcf <iloveyout-mac@qq.com>
2019-07-08 15:48:05 +0800

    创建接口，以及类

--- greeter.js
+++ greeter.js
@@ -1,5 +1,20 @@
+// function greeter(person:string){
+//     return "Hello,"+person;
+// }
+// let user=[0,1,2];
+// document.body.innerHTML=greeter(user);
+var Student = /** @class */ (function () {
+    // 在构造函数的参数上使用public等同于创建了同名的成员变量。
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + ' ' + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
-    return "Hello," + person;
+    return "Hello," + person.firstName + " " + person.lastName;
 }
-var user = 'Jane User';
+var user = new Student('Jane', 'M.', 'User');
 document.body.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,5 +1,28 @@
-    function greeter(person){
-        return "Hello,"+person;
+    // function greeter(person:string){
+    //     return "Hello,"+person;
+    // }
+    // let user=[0,1,2];
+    // document.body.innerHTML=greeter(user);
+
+    class Student{
+        fullName:string;
+        // 在构造函数的参数上使用public等同于创建了同名的成员变量。
+        constructor (public firstName,public middleInitial,public lastName){
+            this.fullName=firstName + ' ' + middleInitial + " " +lastName; 
+        }
     }
-    let user='Jane User';
+    /*
+        接口的定义：interface;
+    */
+    interface Person{
+        firstName:string;
+        lastName:string;
+    }
+
+    function greeter(person:Person){
+        return "Hello," + person.firstName + " " +person.lastName;
+    }
+
+    let user =new Student('Jane', 'M.','User')
+
     document.body.innerHTML=greeter(user);
